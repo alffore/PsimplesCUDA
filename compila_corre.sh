84b6c2cdb228c73d9501b92db9f9ead512b612d3
A. Alfonso F. R. <alffore@gmail.com>
2025-09-20 22:09:06 -0600

    Ajusta script para pruebas

--- compila_corre.sh
+++ compila_corre.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
 nvcc ./src/dispositivos.cu 
 ./a.out
+echo
 
 nvcc ./src/desempeno.cu
 ./a.out 0
+echo
 ./a.out 1
 
-#nvcc ./src/pdisp.cu
-#./a.out
+nvcc ./src/pdisp.cu
+./a.out
+echo
+echo
 
 nvcc ./src/nucleos.cu
 ./a.out 0
+echo
 ./a.out 1
 
+rm ./a.out
